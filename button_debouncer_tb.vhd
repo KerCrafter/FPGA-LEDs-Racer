48002d2fe788b11087afef3552a04602ef0da75e
KerCrafter <clemkerneur@gmail.com>
2024-11-06 09:13:40 +0100

    prepare btn stimulations to simulate interfering signal on press button

--- button_debouncer_tb.vhd
+++ button_debouncer_tb.vhd
@@ -7,12 +7,35 @@ end entity;
 
 architecture behaviour of button_debouncer_tb is
 	signal clk : std_logic;
+	signal btn_in : std_logic;
 begin
 
-	STIM: process
+	CLK_STIM: process
 	begin
 		clk <= '0'; wait for 10 ns;
 		clk <= '1'; wait for 10 ns;
 	end process;
+	
+	BTN_STIM: process
+	begin
+		btn_in <= '0'; wait for 50 ns;
+		
+		btn_in <= '1'; wait for 3 ns;
+		btn_in <= '0'; wait for 3 ns;
+		btn_in <= '1'; wait for 3 ns;
+		btn_in <= '0'; wait for 3 ns;
+		btn_in <= '1'; wait for 3 ns;
+		btn_in <= '0'; wait for 3 ns;
+		btn_in <= '1'; wait for 3 ns;
+		btn_in <= '0'; wait for 3 ns;
+		btn_in <= '1'; wait for 3 ns;
+		btn_in <= '0'; wait for 3 ns;
+		btn_in <= '1'; wait for 3 ns;
+		btn_in <= '0'; wait for 3 ns;
+		btn_in <= '1';
+		
+		wait;
+	end process;
+	
 
 end architecture;
