fc295a158de44c714989374592040a727dce7211
KerCrafter <clemkerneur@gmail.com>
2024-11-06 09:24:57 +0100

    button debouncer : build empty UUT and link to test bench

--- button_debouncer_tb.vhd
+++ button_debouncer_tb.vhd
@@ -5,10 +5,34 @@ use ieee.std_logic_1164.all;
 entity button_debouncer_tb is
 end entity;
 
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity button_debouncer is
+	port (
+		clk : in std_logic;
+		btn_in : in std_logic;
+		btn_debounced : out std_logic
+	);
+end entity;
+
+architecture beh of button_debouncer is
+begin
+
+end architecture;
+
+
 architecture behaviour of button_debouncer_tb is
 	signal clk : std_logic;
 	signal btn_in : std_logic;
+	signal btn_debounced : std_logic;
 begin
+
+	UUT: entity work.button_debouncer port map (
+		clk => clk,
+		btn_in => btn_in,
+		btn_debounced => btn_debounced
+	);
 
 	CLK_STIM: process
 	begin
