e90f9ebd62dde66209e6eb8262f428e88e2b802b
KerCrafter <clemkerneur@gmail.com>
2024-11-07 21:14:46 +0100

    wip btn debouncer, work with a button with a stable press

--- button_debouncer_tb.vhd
+++ button_debouncer_tb.vhd
@@ -7,18 +7,33 @@ end entity;
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity button_debouncer is
 	port (
 		clk : in std_logic;
 		btn_in : in std_logic;
-		btn_debounced : out std_logic
+		btn_debounced : out std_logic := '0'
 	);
 end entity;
 
 architecture beh of button_debouncer is
+	signal cnt : integer range 0 to 2 := 0;
 begin
-	btn_debounced <= '0';
+
+	process(clk)
+	begin
+	
+		if rising_edge(clk) then
+			if btn_in = '1' and cnt /= 2 then
+				cnt <= cnt + 1;
+			end if;
+		end if;
+	
+	end process;
+
+	btn_debounced <= '1' when cnt = 2 else '0';
+	
 end architecture;
 
 
@@ -66,6 +81,25 @@ begin
 		wait until btn_in <= '1';
 		
 		assert btn_debounced = '0' report "just when button is pressed, should be LOW";
+		
+		--one wait one edge clk
+		if clk = '0' then
+			wait until clk = '1';
+		else
+			wait until clk = '0';
+			wait until clk = '1';
+		end if;
+		
+		--one wait one edge clk
+
+		if clk = '0' then
+			wait until clk = '1';
+		else
+			wait until clk = '0';
+			wait until clk = '1';
+		end if;
+		
+		assert btn_debounced = '1' report "debounced button should up to HIGH";
 		
 		wait;
 	
