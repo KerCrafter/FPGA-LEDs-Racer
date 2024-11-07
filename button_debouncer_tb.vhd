99155825b793312ce98c91a4c91e19c6819f6883
KerCrafter <clemkerneur@gmail.com>
2024-11-07 21:51:13 +0100

    add tests to ensure multiples bounces works

--- button_debouncer_tb.vhd
+++ button_debouncer_tb.vhd
@@ -63,27 +63,29 @@ begin
 		--without press bounce
 		btn_in <= '0'; wait for 50 ns;
 		
---		btn_in <= '1'; wait for 3 ns;
---		btn_in <= '0'; wait for 3 ns;
---		btn_in <= '1'; wait for 3 ns;
---		btn_in <= '0'; wait for 3 ns;
---		btn_in <= '1'; wait for 3 ns;
---		btn_in <= '0'; wait for 3 ns;
---		btn_in <= '1'; wait for 3 ns;
---		btn_in <= '0'; wait for 3 ns;
---		btn_in <= '1'; wait for 3 ns;
---		btn_in <= '0'; wait for 3 ns;
---		btn_in <= '1'; wait for 3 ns;
---		btn_in <= '0'; wait for 3 ns;
 		btn_in <= '1'; wait for 50ns;
 		btn_in <= '0'; wait for 50ns;
 		
---		-- with one press bounce
---		btn_in <= '1'; wait for 3 ns;
---		btn_in <= '0'; wait for 3 ns;
---		btn_in <= '1'; wait for 50 ns;
---		
---		btn_in <= '0'; wait for 50 ns;
+		-- with one press bounce
+		btn_in <= '1'; wait for 3 ns;
+		btn_in <= '0'; wait for 3 ns;
+		btn_in <= '1'; wait for 50 ns;
+		
+		btn_in <= '0'; wait for 50 ns;
+		
+		
+		-- with multiples press bounces
+		btn_in <= '1'; wait for 3 ns;
+		btn_in <= '0'; wait for 3 ns;
+		btn_in <= '1'; wait for 3 ns;
+		btn_in <= '0'; wait for 3 ns;
+		btn_in <= '1'; wait for 3 ns;
+		btn_in <= '0'; wait for 3 ns;
+		btn_in <= '1'; wait for 3 ns;
+		btn_in <= '0'; wait for 3 ns;
+		btn_in <= '1'; wait for 50 ns;
+		
+		btn_in <= '0'; wait for 50 ns;
 		
 		wait;
 	end process;
@@ -119,6 +121,77 @@ begin
 
 		assert btn_debounced = '0' report "debounced button should down to LOW when btn released";
 
+		
+		-- 2nd test
+		wait until btn_in <= '1';
+		
+		assert btn_debounced = '0' report "just when button is pressed, should be LOW";
+		
+		--one wait one edge clk
+		if clk = '0' then
+			wait until clk = '1';
+		else
+			wait until clk = '0';
+			wait until clk = '1';
+		end if;
+		
+		--one wait one edge clk
+
+		if clk = '0' then
+			wait until clk = '1';
+		else
+			wait until clk = '0';
+			wait until clk = '1';
+		end if;
+		
+		assert btn_debounced = '1' report "debounced button should up to HIGH";
+		
+		wait until btn_in = '0';
+		wait until clk = '1';
+		wait until clk = '0'; --fix
+
+		assert btn_debounced = '0' report "debounced button should down to LOW when btn released";
+		
+		
+		-- 3rd test
+		wait until btn_in <= '1';
+		
+		assert btn_debounced = '0' report "just when button is pressed, should be LOW";
+		
+		--one wait one edge clk
+		if clk = '0' then
+			wait until clk = '1';
+		else
+			wait until clk = '0';
+			wait until clk = '1';
+		end if;
+		
+		--one wait one edge clk
+
+		if clk = '0' then
+			wait until clk = '1';
+		else
+			wait until clk = '0';
+			wait until clk = '1';
+		end if;
+		
+		--one wait one edge clk
+
+		if clk = '0' then
+			wait until clk = '1';
+		else
+			wait until clk = '0';
+			wait until clk = '1';
+		end if;
+		
+		
+		assert btn_debounced = '1' report "debounced button should up to HIGH";
+		
+		wait until btn_in = '0';
+		wait until clk = '1';
+		wait until clk = '0'; --fix
+
+		assert btn_debounced = '0' report "debounced button should down to LOW when btn released";
 		
 		wait;
 	
