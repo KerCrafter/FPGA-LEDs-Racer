b4769189952089130cda34eb74986f0155a25264
KerCrafter <clemkerneur@gmail.com>
2024-11-13 22:29:12 +0100

    prepare for extraction of WS2812B driver to specific repository

--- LEDs_racer_main.vhd
+++ LEDs_racer_main.vhd
@@ -2,6 +2,34 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+entity LEDs_racer_core is
+	generic (
+		max_pos : integer := 16
+	);
+
+	port(
+		clk : in std_logic;
+		
+		enable : in std_logic;
+		
+		green_input : in std_logic;
+		red_input : in std_logic;
+		blue_input : in std_logic;
+		yellow_input : in std_logic;
+		
+		current_led : in integer range 0 to max_pos-1;
+		led_green_intensity : out integer range 0 to 255;
+		led_red_intensity : out integer range 0 to 255;
+		led_blue_intensity : out integer range 0 to 255;
+		update_frame : out std_logic
+		
+	);
+end entity;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
 entity LEDs_racer_main is
 	generic (
 		max_pos : integer := 16
@@ -29,7 +57,53 @@ entity LEDs_racer_main is
 end entity;
 
 architecture structural of LEDs_racer_main is
+	signal led_proceed : integer range 0 to max_pos-1;
+	
+	signal update_frame : std_logic;
+	
+	signal red_intensity : integer range 0 to 255;
+	signal blue_intensity : integer range 0 to 255;
+	signal green_intensity : integer range 0 to 255;
+begin
 
+	
+	WS2812B_driver: entity work.WS2812B_driver
+		generic map(max_pos => max_pos)
+		port map(
+			clk => clk,
+			leds_line => leds_line,
+			enable => enable,
+			
+			program_led_number => led_proceed,
+			program_red_intensity => red_intensity,
+			program_blue_intensity => blue_intensity,
+			program_green_intensity => green_intensity,
+			update_frame => update_frame
+		);
+		
+	LEDs_racer_core: entity work.LEDs_racer_core
+		generic map(max_pos => max_pos)
+		port map(
+			clk => clk,
+			enable => enable,
+
+			update_frame => update_frame,
+			
+			green_input => green_input,
+			red_input => red_input,
+			blue_input => blue_input,
+			yellow_input => yellow_input,
+		
+			current_led => led_proceed,
+			led_green_intensity => green_intensity,
+			led_red_intensity => red_intensity,
+			led_blue_intensity => blue_intensity
+		);
+
+	
+end architecture;
+
+architecture structural of LEDs_racer_core is
 	signal green_input_debounced : std_logic;
 
 	signal red_cur_pos : integer range 0 to max_pos-1;
@@ -41,16 +115,8 @@ architecture structural of LEDs_racer_main is
 	signal blue_activity : std_logic;
 	signal green_activity : std_logic;
 	signal yellow_activity : std_logic;
-	
-	signal red_intensity : integer range 0 to 255;
-	signal blue_intensity : integer range 0 to 255;
-	signal green_intensity : integer range 0 to 255;
-	
-	signal led_proceed : integer range 0 to max_pos-1;
-	
-	signal update_frame : std_logic;
-
 begin
+
 	red_btn: entity work.player_button
 		generic map(max_pos => max_pos)
 		port map (
@@ -101,21 +167,7 @@ begin
 		
 		R => update_frame
 	);
-	
-	WS2812B_driver: entity work.WS2812B_driver
-		generic map(max_pos => max_pos)
-		port map(
-			clk => clk,
-			leds_line => leds_line,
-			enable => enable,
-			
-			program_led_number => led_proceed,
-			program_red_intensity => red_intensity,
-			program_blue_intensity => blue_intensity,
-			program_green_intensity => green_intensity,
-			update_frame => update_frame
-		);
-	
+
 	WS2812B_gameplay_program: entity work.WS2812B_gameplay_program
 		generic map(max_pos => max_pos)
 		port map(
@@ -124,19 +176,12 @@ begin
 			green_pos => green_cur_pos,
 			yellow_pos => yellow_cur_pos,
 			
-			led_number => led_proceed,
+			led_number => current_led,
 		
-			green_intensity => green_intensity,
-			red_intensity => red_intensity,
-			blue_intensity => blue_intensity
+			green_intensity => led_green_intensity,
+			red_intensity => led_red_intensity,
+			blue_intensity => led_blue_intensity
 		);
-		
-	dp_0 <= led_proceed;
-	
-	dp_green <= green_intensity;
-	dp_red <= red_intensity;
-	dp_blue <= blue_intensity;
-	
-	dp_red_cur_pos <= red_cur_pos;
-	
+
+
 end architecture;
