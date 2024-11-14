08f0f06340898ddd03ea60d8cc2d5386961ecef4
KerCrafter <clemkerneur@gmail.com>
2024-11-14 09:12:50 +0100

    WS2812B-driver as submodule

--- gui_run_simulation_WS2812B_driver_tb.bat
+++
@@ -1,1 +0,0 @@
-vsim -t 1ps -do "vlib work; vcom -93 -work work ./WS2812B_driver.vhd; vcom -93 -work work ./WS2812B_driver_tb.vhd; vsim WS2812B_driver_tb; vsim WS2812B_driver_tb; add wave *; view structure; view signals; run -all"
--- run_simulation_WS2812B_driver_tb.bat
+++
@@ -1,1 +0,0 @@
-vsim -t 1ps -c -do "vlib work; vcom -93 -work work ./WS2812B_driver.vhd; vcom -93 -work work ./WS2812B_driver_tb.vhd; vsim WS2812B_driver_tb; vsim WS2812B_driver_tb; run -all"
