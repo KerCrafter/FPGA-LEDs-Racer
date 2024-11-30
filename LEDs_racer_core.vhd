947a1dcbaf7628d996faf605ce9b074ea78ba480
KerCrafter <clemkerneur@gmail.com>
2024-11-30 12:31:16 +0100

    move screen_manager in his own entity file

--- LEDs_racer_core.vhd
+++ LEDs_racer_core.vhd
@@ -24,31 +24,6 @@ entity LEDs_racer_core is
 	);
 end entity;
 
-library ieee;
-use ieee.std_logic_1164.all;
-
-entity screen_manager is
-	generic (
-		max_pos : integer := 109
-	);
-
-	port(
-		green_cur_pos : in integer range 0 to max_pos-1;
-		red_cur_pos : in integer range 0 to max_pos-1;
-		blue_cur_pos : in integer range 0 to max_pos-1;
-		yellow_cur_pos : in integer range 0 to max_pos-1;
-	
-		game_in_progress_screen : out std_logic := '1';
-		game_finished_screen : out std_logic := '0'
-	);
-end entity;
-
-architecture behaviour of screen_manager is
-begin
-	game_finished_screen <= '1' when green_cur_pos = max_pos-1 or red_cur_pos = max_pos-1 or blue_cur_pos = max_pos-1 or yellow_cur_pos = max_pos-1 else '0';
-	game_in_progress_screen <= '1' when green_cur_pos < max_pos-1 and red_cur_pos < max_pos-1 and blue_cur_pos < max_pos-1 and yellow_cur_pos < max_pos-1 else '0';
-end architecture;
-
 architecture structural of LEDs_racer_core is
 	signal red_cur_pos : integer range 0 to max_pos-1;
 	signal blue_cur_pos : integer range 0 to max_pos-1;
