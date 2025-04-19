f369e56976d7920e9b857bb39ea0c17eb5b11203
KerCrafter <clemkerneur@gmail.com>
2025-04-19 15:30:11 +0200

    is_in_menu detect (hardcoded) when blue & red ready to play

--- screen_manager/is_in_menu.vhd
+++ screen_manager/is_in_menu.vhd
@@ -10,11 +10,23 @@ entity is_in_menu is
     blue_ready_to_play : in std_logic;
     yellow_ready_to_play : in std_logic;
   
-    result : out std_logic
+    result : out std_logic;
+    D_two_players_ready : out std_logic
   );
 end entity;
 
 architecture behaviour of is_in_menu is
+  signal two_players_ready : std_logic := '0';
 begin
+  process ( clk )
+  begin
+    if rising_edge(clk) then
+      if blue_ready_to_play = '1' and red_ready_to_play = '1' then
+        two_players_ready <= '1';
+      end if;
+    end if;
+  end process;
+
   result <= '0';
+  D_two_players_ready <= two_players_ready;
 end architecture;
