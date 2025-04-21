a993e6463f37e4282b9ca43f8f2d63aa24e6ef54
KerCrafter <clemkerneur@gmail.com>
2025-04-21 22:26:26 +0200

    embed router in component

--- screen_manager.vhd
+++ screen_manager.vhd
@@ -18,9 +18,9 @@ entity screen_manager is
     yellow_ready_to_play : in std_logic;
     yellow_cur_pos : in integer range 0 to max_pos-1;
   
-    menu_screen : buffer std_logic := '0';
-    game_in_progress_screen : buffer std_logic := '1';
-    game_finished_screen : buffer std_logic := '0';
+    menu_screen : out std_logic := '0';
+    game_in_progress_screen : out std_logic := '1';
+    game_finished_screen : out std_logic := '0';
     current_screen : out std_logic_vector(1 downto 0)
   );
 end entity;
@@ -28,45 +28,25 @@ end entity;
 architecture structural of screen_manager is
 begin
 
-  is_in_menu: entity work.is_in_menu
+  router: entity work.router
+    generic map(max_pos => max_pos)
     port map (
       clk => clk,
 
       green_ready_to_play => green_ready_to_play,
+      green_cur_pos => green_cur_pos,
       red_ready_to_play => red_ready_to_play,
+      red_cur_pos => red_cur_pos,
       blue_ready_to_play => blue_ready_to_play,
+      blue_cur_pos => blue_cur_pos,
       yellow_ready_to_play => yellow_ready_to_play,
-
-      result => menu_screen
-    );
-
-  is_game_finished : entity work.is_game_finished
-    generic map(max_pos => max_pos)
-    port map (
-      is_in_menu => menu_screen,
-      green_cur_pos => green_cur_pos,
-      red_cur_pos => red_cur_pos,
-      blue_cur_pos => blue_cur_pos,
       yellow_cur_pos => yellow_cur_pos,
-
-      result => game_finished_screen
+    
+      menu_screen => menu_screen,
+      game_in_progress_screen => game_in_progress_screen,
+      game_finished_screen => game_finished_screen,
+      current_screen => current_screen
     );
 
-  is_game_started : entity work.is_game_started
-    generic map(max_pos => max_pos)
-    port map (
-      is_in_menu => menu_screen,
-      green_cur_pos => green_cur_pos,
-      red_cur_pos => red_cur_pos,
-      blue_cur_pos => blue_cur_pos,
-      yellow_cur_pos => yellow_cur_pos,
-
-      result => game_in_progress_screen
-    );
-
-  current_screen <= "00" when menu_screen = '1' else
-                    "01" when game_in_progress_screen = '1' else
-                    "10" when game_finished_screen = '1' else
-                    "11";
 
 end architecture;
---
+++ screen_manager/router.vhd
@@ -0,0 +1,72 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity router is
+  generic (
+    max_pos : integer := 109
+  );
+
+  port(
+    clk : in std_logic;
+
+    green_ready_to_play : in std_logic;
+    green_cur_pos : in integer range 0 to max_pos-1;
+    red_ready_to_play : in std_logic;
+    red_cur_pos : in integer range 0 to max_pos-1;
+    blue_ready_to_play : in std_logic;
+    blue_cur_pos : in integer range 0 to max_pos-1;
+    yellow_ready_to_play : in std_logic;
+    yellow_cur_pos : in integer range 0 to max_pos-1;
+  
+    menu_screen : buffer std_logic;
+    game_in_progress_screen : buffer std_logic;
+    game_finished_screen : buffer std_logic;
+    current_screen : out std_logic_vector(1 downto 0)
+  );
+end entity;
+
+architecture structural of router is
+begin
+
+  is_in_menu: entity work.is_in_menu
+    port map (
+      clk => clk,
+
+      green_ready_to_play => green_ready_to_play,
+      red_ready_to_play => red_ready_to_play,
+      blue_ready_to_play => blue_ready_to_play,
+      yellow_ready_to_play => yellow_ready_to_play,
+
+      result => menu_screen
+    );
+
+  is_game_finished : entity work.is_game_finished
+    generic map(max_pos => max_pos)
+    port map (
+      is_in_menu => menu_screen,
+      green_cur_pos => green_cur_pos,
+      red_cur_pos => red_cur_pos,
+      blue_cur_pos => blue_cur_pos,
+      yellow_cur_pos => yellow_cur_pos,
+
+      result => game_finished_screen
+    );
+
+  is_game_started : entity work.is_game_started
+    generic map(max_pos => max_pos)
+    port map (
+      is_in_menu => menu_screen,
+      green_cur_pos => green_cur_pos,
+      red_cur_pos => red_cur_pos,
+      blue_cur_pos => blue_cur_pos,
+      yellow_cur_pos => yellow_cur_pos,
+
+      result => game_in_progress_screen
+    );
+
+  current_screen <= "00" when menu_screen = '1' else
+                    "01" when game_in_progress_screen = '1' else
+                    "10" when game_finished_screen = '1' else
+                    "11";
+
+end architecture;
