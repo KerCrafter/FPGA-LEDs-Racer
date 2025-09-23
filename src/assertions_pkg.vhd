a987126013329a433d149b4c8866a957c5d3b1fc
KerCrafter <clemkerneur@gmail.com>
2025-09-23 11:45:57 +0200

    procedure assert_all_LEDs_should_be_GREEN move to package

--- src/assertions_pkg.vhd
+++ src/assertions_pkg.vhd
@@ -27,6 +27,12 @@ package assertions_pkg is
     signal test_status: out t_TEST_STATUS
   );
 
+  procedure assert_all_LEDs_should_be_GREEN(
+    signal sut: in LEDs_racer_core_sut_interface;
+    signal current_led: out integer range 0 to 108;
+    signal test_status: out t_TEST_STATUS
+  );
+
 
 end package;
 
@@ -73,6 +79,19 @@ package body assertions_pkg is
 
     for led_number in 0 to 108 loop
       wait for 20 ns; current_led <= led_number; wait for 1 ps; assert_GRB(sut, test_status, 0, 10, 0, "LED " & to_string(led_number) & ": should be RED");
+    end loop;
+
+  end procedure;
+
+  procedure assert_all_LEDs_should_be_GREEN(
+    signal sut: in LEDs_racer_core_sut_interface;
+    signal current_led: out integer range 0 to 108;
+    signal test_status: out t_TEST_STATUS
+  ) is
+  begin
+
+    for led_number in 0 to 108 loop
+      wait for 20 ns; current_led <= led_number; wait for 1 ps; assert_GRB(sut, test_status, 10, 0, 0, "LED " & to_string(led_number) & ": should be GREEN");
     end loop;
 
   end procedure;
--- src/simulations/LEDs_racer_core_green_wins_sim.vhd
+++ src/simulations/LEDs_racer_core_green_wins_sim.vhd
@@ -3,7 +3,8 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.test_status_pkg.all;
 use work.player_interactions_test_pkg.all;
-use work.players_commands_pkg.all;
+use work.sut_pkg.all;
+use work.assertions_pkg.all;
 
 entity LEDs_racer_core_green_wins_sim is
   port (
@@ -12,102 +13,48 @@ entity LEDs_racer_core_green_wins_sim is
 end entity;
 
 architecture simulation of LEDs_racer_core_green_wins_sim is
-  signal clk : std_logic := '0';
-
-  signal players_commands : t_PLAYERS_COMMANDS := PLAYERS_COMMANDS_INIT;
-  
-  signal current_led : integer range 0 to 4;
-  signal led_green_intensity : std_logic_vector(7 downto 0);
-  signal led_red_intensity : std_logic_vector(7 downto 0);
-  signal led_blue_intensity : std_logic_vector(7 downto 0);
-  
-  signal update_frame : std_logic;
+  signal SUT : LEDs_racer_core_sut_interface;
 begin
-  UUT: entity work.LEDs_racer_core
-    generic map(max_pos => 5)
+  UUT: entity work.LEDs_racer_core_SUT
     port map (
-      clk => clk,
-
-      players_commands => players_commands,
-
-      current_led => current_led,
-      led_green_intensity => led_green_intensity,
-      led_red_intensity => led_red_intensity,
-      led_blue_intensity => led_blue_intensity,
-      update_frame => update_frame
+      sut_interface => SUT
     );
   
-  PLAYS_STIM: process
-
-    procedure assert_GRB(
-      led_green_intensity_i: integer range 0 to 255;
-      led_red_intensity_i: integer range 0 to 255;
-      led_blue_intensity_i: integer range 0 to 255;
-      report_message: string
-    ) is
-    begin
-
-      if led_green_intensity = std_logic_vector(to_unsigned(led_green_intensity_i, 8)) and led_red_intensity = std_logic_vector(to_unsigned(led_red_intensity_i, 8)) and led_blue_intensity = std_logic_vector(to_unsigned(led_blue_intensity_i, 8)) then
-      else
-        SIMULATION_FAIL(test_status);
-      end if;
-
-      assert led_green_intensity = std_logic_vector(to_unsigned(led_green_intensity_i, 8)) and led_red_intensity = std_logic_vector(to_unsigned(led_red_intensity_i, 8)) and led_blue_intensity = std_logic_vector(to_unsigned(led_blue_intensity_i, 8)) report report_message;
-    end procedure;
-    
-    procedure assert_all_LEDs_should_be_GREEN is
-    begin
-      wait for 20 ns; current_led <= 0; wait for 1 ps; assert_GRB(10, 0, 0, "LED 0 : should be GREEN");
-      wait for 20 ns; current_led <= 1; wait for 1 ps; assert_GRB(10, 0, 0, "LED 1 : should be GREEN");
-      wait for 20 ns; current_led <= 2; wait for 1 ps; assert_GRB(10, 0, 0, "LED 2 : should be GREEN");
-      wait for 20 ns; current_led <= 3; wait for 1 ps; assert_GRB(10, 0, 0, "LED 3 : should be GREEN");
-      wait for 20 ns; current_led <= 4; wait for 1 ps; assert_GRB(10, 0, 0, "LED 4 : should be GREEN");
-    end procedure;
-
+  SIMULATION: process
   begin
-    player_press_his_button_during(20 ns, players_commands.green); --GREEN Player, UP from position 0 to 1
-    player_press_his_button_during(20 ns, players_commands.green); --GREEN Player, UP from position 1 to 2
-    player_press_his_button_during(20 ns, players_commands.green); --GREEN Player, UP from position 2 to 3
-    player_press_his_button_during(20 ns, players_commands.green); --GREEN Player, UP from position 3 to 4
+    for k in 1 to 109 loop
+      player_press_his_button_during(20 ns, SUT.players_commands.green);
+    end loop;
     
-    assert_all_LEDs_should_be_GREEN;
+    assert_all_LEDs_should_be_GREEN(SUT, SUT.current_led, test_status);
 
     -- Game is END Should lock GREEN actions
-    player_press_his_button_during(20 ns, players_commands.green); --GREEN player STAY in position 4
+    player_press_his_button_during(20 ns, SUT.players_commands.green); --GREEN player STAY in position 4
 
-    assert_all_LEDs_should_be_GREEN;
+    assert_all_LEDs_should_be_GREEN(SUT, SUT.current_led, test_status);
 
     --Try move the RED player
-    player_press_his_button_during(20 ns, players_commands.red);
-    player_press_his_button_during(20 ns, players_commands.red);
-    player_press_his_button_during(20 ns, players_commands.red);
-    player_press_his_button_during(20 ns, players_commands.red);
+    for k in 1 to 109 loop
+      player_press_his_button_during(20 ns, SUT.players_commands.red);
+    end loop;
 
-    assert_all_LEDs_should_be_GREEN;
+    assert_all_LEDs_should_be_GREEN(SUT, SUT.current_led, test_status);
 
     --Try move the BLUE player
-    player_press_his_button_during(20 ns, players_commands.blue);
-    player_press_his_button_during(20 ns, players_commands.blue);
-    player_press_his_button_during(20 ns, players_commands.blue);
-    player_press_his_button_during(20 ns, players_commands.blue);
+    for k in 1 to 109 loop
+      player_press_his_button_during(20 ns, SUT.players_commands.blue);
+    end loop;
 
-    assert_all_LEDs_should_be_GREEN;
+    assert_all_LEDs_should_be_GREEN(SUT, SUT.current_led, test_status);
 
     --Try move the YELLOW player
-    player_press_his_button_during(20 ns, players_commands.yellow);
-    player_press_his_button_during(20 ns, players_commands.yellow);
-    player_press_his_button_during(20 ns, players_commands.yellow);
-    player_press_his_button_during(20 ns, players_commands.yellow);
+    for k in 1 to 109 loop
+      player_press_his_button_during(20 ns, SUT.players_commands.yellow);
+    end loop;
 
-    assert_all_LEDs_should_be_GREEN;
+    assert_all_LEDs_should_be_GREEN(SUT, SUT.current_led, test_status);
 
     SIMULATION_END(test_status);
   end process;
   
-  CLK_STIM: process
-  begin
-    -- Inverse CLK every 10ns = 20ns per cycle = 50MHz
-    clk <= not clk; wait for 10 ns;
-  end process;
-
 end architecture;
