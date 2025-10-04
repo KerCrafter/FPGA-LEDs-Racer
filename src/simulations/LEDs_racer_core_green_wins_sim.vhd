8563b11f526ce0d997cbc50996005fba637bc89c
KerCrafter <clemkerneur@gmail.com>
2025-10-04 21:40:49 +0200

    refactor direct access to gameplay in green wins test

--- src/simulations/LEDs_racer_core_green_wins_sim.vhd
+++ src/simulations/LEDs_racer_core_green_wins_sim.vhd
@@ -22,6 +22,14 @@ begin
   
   SIMULATION: process
   begin
+
+    wait_until_gameplay_start_for_2_players(
+      player1 => SUT.players_commands.green,
+      player2 => SUT.players_commands.yellow,
+      clk => SUT.clk,
+      opt_with_menu => SUT.opt_with_menu
+    );
+
     for k in 1 to 109 loop
       player_press_his_button_during(20 ns, SUT.players_commands.green);
     end loop;
