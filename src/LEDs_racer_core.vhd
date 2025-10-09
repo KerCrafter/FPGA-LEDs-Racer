ff5d46e5a756e5aa46ac39ee1d0c0ccf0134ef61
KerCrafter <clemkerneur@gmail.com>
2025-10-09 21:15:17 +0200

    encapuslate end game logics into entity

--- src/LEDs_racer_core.vhd
+++ src/LEDs_racer_core.vhd
@@ -1,5 +1,40 @@
 library ieee;
 use ieee.std_logic_1164.all;
+
+entity end_game_logics is
+  generic (
+    END_TIMER_CLK_COUNT : integer
+  );
+
+  port(
+    clk : in std_logic;
+    current_screen : in std_logic_vector(1 downto 0);
+
+    trigger_reset_all : out std_logic
+  );
+end entity;
+
+architecture rtl of end_game_logics is
+  signal end_timer_enable : std_logic;
+begin
+
+  TIMER: entity work.timer
+    generic map(
+      CLK_COUNT => END_TIMER_CLK_COUNT,
+      FIRST_TICK_AFTER_DELAY => true
+    )
+    port map(
+      clk => clk,
+      enable => end_timer_enable,
+      tick => trigger_reset_all
+    );
+
+  end_timer_enable <= '1' when current_screen = "10" else '0';
+
+end architecture;
+
+library ieee;
+use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.players_commands_pkg.all;
 
@@ -43,7 +78,6 @@ architecture structural of LEDs_racer_core is
   signal is_in_menu : std_logic;
   signal countdown : integer range 0 to 7;
 
-  signal end_timer_enable : std_logic;
   signal reset_all : std_logic;
 begin
   red_btn: entity work.player_button
@@ -111,17 +145,6 @@ begin
       countdown => countdown,
       activity => menu_activity
     );
-
-  end_timer: entity work.timer
-    generic map(
-      CLK_COUNT => END_TIMER_CLK_COUNT,
-      FIRST_TICK_AFTER_DELAY => true
-    )
-    port map(
-      clk => clk,
-      enable => end_timer_enable,
-      tick => reset_all
-    );
   
   activity_detector: entity work.activity_detector port map(
     clk => clk,
@@ -161,6 +184,12 @@ begin
       led_blue_intensity => led_blue_intensity
     );
 
-  end_timer_enable <= '1' when current_screen = "10" else '0';
+  end_game_logics : entity work.end_game_logics
+    generic map(END_TIMER_CLK_COUNT => END_TIMER_CLK_COUNT)
+    port map(
+      clk => clk,
+      current_screen => current_screen,
+      trigger_reset_all => reset_all
+    );
 
 end architecture;
