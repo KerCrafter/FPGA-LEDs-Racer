63c07bd7afa9b0ce1338a87690e90247e5d0130e
KerCrafter <clemkerneur@gmail.com>
2025-10-11 22:53:26 +0200

    vunit runner as shared dependency

--- run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo docker run --rm \
-  -v /$(pwd)://work \
-  -w //work \
-  ghdl/vunit:llvm-master sh -c ' \
-    VUNIT_SIMULATOR=ghdl; \
-    for f in $(find ./ -name 'run.py'); do python3 $f; done \
-  '
