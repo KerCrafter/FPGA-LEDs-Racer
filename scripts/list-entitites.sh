bd7d256ff65fc09d0d0f76d8a552e420ba122cec
KerCrafter <clemkerneur@gmail.com>
2025-10-12 22:14:27 +0200

    list entities script imprve output

--- scripts/list-entitites.sh
+++ scripts/list-entitites.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep -E -r "entity (.+) is" --include='*.vhd' --exclude '*_tb.vhd'
+grep -E -r "entity (.+) is" --include='*.vhd' --exclude '*_tb.vhd' | sed 's/entity //' | sed 's/ is//' | sed 's/:/ => /'
