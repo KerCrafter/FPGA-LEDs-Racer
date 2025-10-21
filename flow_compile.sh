aa67e6f41ebea2a6d249a2a11348bc7e7e25d6f4
KerCrafter <clemkerneur@gmail.com>
2025-10-21 20:49:45 +0200

    flow compile refacto

--- flow_compile.sh
+++ flow_compile.sh
@@ -1,1 +1,1 @@
-sudo docker run -w `pwd` -v `pwd`:`pwd` --rm -it quartus24.1 /24.1std/quartus/bin/quartus_sh --flow compile LEDs-racer.qpf -c DE0_NANO_LEDs_racer_main
+sudo docker run -w `pwd` -v `pwd`:`pwd` --rm -it quartus24.1 --flow compile LEDs-racer.qpf -c DE0_NANO_LEDs_racer_main
