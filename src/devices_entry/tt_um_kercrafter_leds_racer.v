f03f0ead93f7253175af95408b0f66bf192f0107
KerCrafter <clemkerneur@gmail.com>
2025-10-30 11:00:17 +0100

    tt vhdl

---
+++ src/devices_entry/tt_um_kercrafter_leds_racer.v
@@ -0,0 +1,20 @@
+`default_nettype none
+
+module tt_um_kercrafter_leds_racer (
+    input wire CLK,
+    input wire GREEN_BTN,
+    input wire RED_BTN,
+    input wire BLUE_BTN,
+    input wire YELLOW_BTN,
+    input wire FORCE_RESET,
+    output wire LEDS_LINE,
+    output wire TP_SCREEN_0,
+    output wire TP_SCREEN_1,
+    output wire TP_BLUE_RTP,
+    output wire TP_RED_RTP,
+    output wire TP_GREEN_RTP,
+    output wire TP_YELLOW_RTP,
+    output wire TP_UPDATE_FRAME
+);
+
+endmodule
